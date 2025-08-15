7bcbbdfb4aef11deea9aad525b6c6cfa989bf245
Samay <samaypowade9@gmail.com>
2025-08-15 23:53:03 +0530

    THe main changes of work

--- main.py
+++ main.py
@@ -49,9 +49,69 @@
 #     else:
 #         return {"error": "ID mismatch"}
 
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+# get , post , put , delete 
 
 
 
 
+app = FastAPI()
+
+students = {
+    1: {"name": "Samay", "age": 21},
+    2: {"name": "Aarav", "age": 22}
+}
+
+@app.get("/")
+def welcome():
+    return {"Welcome to the backend lecture...."}
+
+
+# @app.get("/show")
+# def show():
+#     return students
+
+
+
+@app.get("/show")
+def seedata(id : int):
+    return students.get(id)
+
+# pydantic 
+
+
+class Students(BaseModel):
+    id:int
+    name:str
+    age:int
+
+
+@app.post("/show")
+def addData(data:Students):
+    students[data.id] = {"name":data.name, "age":data.age}
+    return students
+
+@app.get("/seealldata")
+def showone():
+    return students
+
+
+
+@app.put("/show")
+def update_method(id:int,data:Students):
+    if id in students:
+        students[id] = data.dict()
+        return "Data is updated"
+    return "Id is not found"
+    
+
+
+@app.delete("/show")
+def delete(id : int):
+    if id in students:
+        del students[id]
+        return "Deleted the data"
 
 
